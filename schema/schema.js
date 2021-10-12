5a14b417a4ba6be7bb1d1632d3d20c163594d98d
Masho Ado <mashoado@gmail.com>
2021-10-12 22:04:59 +0300

    add books and authors query fields of RootQueryType

--- schema/schema.js
+++ schema/schema.js
@@ -69,6 +69,18 @@ const RootQueryType = new GraphQLObjectType({
             resolve(parent,args){
                 return _.find(authors, {id:args.id})
             }
+        },
+        books:{
+            type: new GraphQLList(BookType),
+            resolve(parent,args){
+                return books
+            }
+        },
+        authors:{
+            type: new GraphQLList(AuthorType),
+            resolve(parent,args){
+                return authors
+            }
         }
     }
 })
